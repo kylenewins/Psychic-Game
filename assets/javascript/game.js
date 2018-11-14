969912f888d84d4f41ef97d7ec998c7b6cb16601
Toad Sage <kylenewins@gmail.com>
2018-11-14 11:21:22 -0500

    logic push

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+var computerChoice = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+
+
+var wins=0;
+var losses=0;
+var guesses=9;
+
+var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+document.onkeyup=function(event){
+    var userGuess = event.key;
+    
+    if(guesses===0){
+        losses++;
+        guesses=9;
+        computerGuess= computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    }
+    else if(userGuess===computerGuess){
+        wins++;
+        guesses=9;
+        computerGuess= computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+    }
+    else if(!(userGuess===computerGuess)){
+        guesses--;
+    }
+   
+
+    function test(){
+        console.log("CG: " + computerGuess);
+        console.log("UG: " + userGuess);
+        console.log("guesses: " + guesses);
+        console.log("losses: " + losses);
+        console.log("wins: " + wins);
+    }
+    test()
+}
+
+
+
